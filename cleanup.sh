b16bba57d20460e2db16451b0471d2612488c314
R vd Hoek <ralvdhoek@gmail.com>
2014-03-26 19:41:50 +0100

    Create cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+for file in /var/log/*.log /var/log/mail.* /var/log/debug /var/log/syslog /var/log/wtmp; do [ -f "$file" ]  && rm -f "$file"; done
+for dir in fsck news; do [ -d "/var/log/$dir" ] && rm -rf "/var/log/$dir"; done
+for file in /var/log/apache2/*.log /var/log/mrtg/*.log /var/log/*.log.* /var/log/*.gz /var/log/*.1 /var/log/dmesg.* /var/log/dmesg /var/log/messages.* /var/log/messages;  do [ -f "$file" ] && rm -f "$file"; done
+rm /var/www/mrtg/*.png
+apt-get -y update > /dev/null
+apt-get -y dist-upgrade
+apt-get -y autoremove > /dev/null
+apt-get -y autoclean > /dev/null
+
+HOSTNAME=$(hostname)
+echo  "Host: $HOSTNAME is cleaned/updated" | mail -s "Host: $HOSTNAME is cleaned/updated" email@gmail.com
+#echo 0
+TODAY=$(date)
+echo "Date: $TODAY                     Host:$HOSTNAME             " >> /var/log/clean_script.log
+echo "----------------------------------------------------------------------------" >> /var/log/clean_script.log
+
