128159b0934520c4f80af8d40946f42ad3c5e6f2
R vd Hoek <ralvdhoek@gmail.com>
2014-04-15 18:59:24 +0200

    Update cleanup.sh
    Use "-q" with apt-get

--- cleanup.sh
+++ cleanup.sh
@@ -3,10 +3,10 @@ for file in /var/log/*.log /var/log/mail.* /var/log/debug /var/log/syslog /var/l
 for dir in fsck news; do [ -d "/var/log/$dir" ] && rm -rf "/var/log/$dir"; done
 for file in /var/log/apache2/*.log /var/log/mrtg/*.log /var/log/*.log.* /var/log/*.gz /var/log/*.1 /var/log/dmesg.* /var/log/dmesg /var/log/messages.* /var/log/messages;  do [ -f "$file" ] && rm -f "$file"; done
 rm /var/www/mrtg/*.png
-apt-get -y update > /dev/null
-apt-get -y dist-upgrade
-apt-get -y autoremove > /dev/null
-apt-get -y autoclean > /dev/null
+apt-get -y update -q
+apt-get -y dist-upgrade -q
+apt-get -y autoremove -q
+apt-get -y autoclean -q
 
 HOSTNAME=$(hostname)
 echo  "Host: $HOSTNAME is cleaned/updated" | mail -s "Host: $HOSTNAME is cleaned/updated" email@gmail.com
