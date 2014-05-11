7f78ea0c8fac6969e4ba0d3dd13050ddd87c173e
R vd Hoek <ralvdhoek@gmail.com>
2014-05-12 00:34:37 +0200

    Update cleanup.sh
    Two clean commands added

--- cleanup.sh
+++ cleanup.sh
@@ -6,7 +6,7 @@ CLEARCOMMAND="truncate --size 0 "
 for file in /var/log/*.log /var/log/mail.* /var/log/debug /var/log/syslog /var/log/wtmp; do [ -f "$file" ]  && $CLEARCOMMAND "$file"; done
 for dir in fsck news; do [ -d "/var/log/$dir" ] && rm -rf "/var/log/$dir"; done
 for file in /var/log/apache2/*.log /var/log/mrtg/*.log /var/log/*.log.* /var/log/*.gz /var/log/*.1 /var/log/dmesg.* /var/log/dmesg /var/log/messages.* /var/log/messages;  do [ -f "$file" ] && $CLEARCOMMAND "$file"; done
-$CLEARCOMMAND /var/www/mrtg/*.png
+
 apt-get -y update -qq
 apt-get -y dist-upgrade -qq
 apt-get -y autoremove -qq
